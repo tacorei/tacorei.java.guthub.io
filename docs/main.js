730a48ecf59a477ddff91400bb3e674d897f05d9
tacorei <0102520zarusobaoshi@gmail.com>
2024-03-31 11:42:58 +0900

    no message

--- docs/main.js
+++ docs/main.js
@@ -1,8 +1,7 @@
 'use strict';
 
 /*alert("hello world!");*/
-console.log(1+1-5/5*25+30)
-console.log(68+49)
-console.log(89+24)
-console.log(45+29)
-console.log(4.5e-3-5)
+alert("45-46 31-29 78-56")
+alert("23+12 -23+76 35.97+0.25")
+
+
